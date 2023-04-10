beb9ccbacbbbd0253a9e85fad5f475ac7344d0c4
진동현 <vibration727@naver.com>
2023-04-10 09:33:24 +0900

    study_project

---
+++ src/main/java/board/HomeController.java
@@ -0,0 +1,22 @@
+package board;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
+
+@Controller
+public class HomeController {
+
+	@GetMapping("/")
+	public String home(Model model) {
+		model.addAttribute("title","zip");
+		model.addAttribute("message","welcom to the home page!");
+		return "home";
+	}
+	
+	@GetMapping("/hello")
+	public String hello(Model model) {
+		model.addAttribute("title","hello");
+		return "hello";
+	}
+}
---
+++ src/test/java/board/UserboardApplicationTests.java
@@ -0,0 +1,13 @@
+package board;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
